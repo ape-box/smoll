68b84918c8d24acdb9785f7bc78d9f2123de09ef
Alessio Peternelli <alessio.peternelli@gmail.com>
2018-02-15 18:14:04 +0000

    Exploring with backbone

---
+++ Smoll.Api.Back.Client/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Smoll.Api.Back.Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
