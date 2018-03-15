47acfed038c1a1c6e6e7f6e9cb2b44302b068aa1
Alessio Peternelli <alessio.peternelli@gmail.com>
2018-03-15 09:18:18 +0000

    Test configuration

--- build.sh
+++ build.sh
@@ -6,8 +6,8 @@ dotnet restore
 
 dotnet test .\Smoll.Api.Back.Tests\Smoll.Api.Back.Tests.csproj
 
-dotnet test .\Smoll.Api.Back.Tests\Smoll.Api.Common.Tests.csproj
+# dotnet test .\Smoll.Api.Back.Tests\Smoll.Api.Common.Tests.csproj
 
-dotnet test .\Smoll.Api.Back.Tests\Smoll.Api.Front.Tests.csproj
+# dotnet test .\Smoll.Api.Back.Tests\Smoll.Api.Front.Tests.csproj
 
 dotnet build
